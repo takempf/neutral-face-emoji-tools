d34b70943fa22c1bdaabfdf61b3fc6d9c2edd32a
Aarni Koskela <akx@iki.fi>
2024-12-27 11:24:37 +0200

    Fix build to work with modern Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
-import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import css from "rollup-plugin-import-css";
 import json from '@rollup/plugin-json';
-import globals from 'rollup-plugin-node-globals';
-import builtins from 'rollup-plugin-node-builtins';
-import embedCSS from 'rollup-plugin-embed-css';
+import resolve from '@rollup/plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
 
 const config = {
   input: 'src/content.js',
@@ -14,22 +11,15 @@ const config = {
     format: 'iife'
   },
   plugins: [
-    embedCSS(),
-    svelte({
-      dev: true
-    }),
-    babel({
-      babelHelpers: 'bundled'
-    }),
+    css({inject: true}),
+    svelte({compilerOptions: {dev: true}}),
     resolve({
-			browser: true,
-			dedupe: ['svelte'],
-			preferBuiltins: true,
-		}),
-		commonjs(),
+      browser: true,
+      dedupe: ['svelte'],
+      preferBuiltins: true,
+    }),
+    commonjs(),
     json(),
-		globals(),
-		builtins()
   ]
 };
 
