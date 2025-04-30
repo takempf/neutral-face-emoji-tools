be016c7c53d16f18bc12e4e13f5c9eaddb1e038c
Timothy Kempf <tim@kempf.dev>
2025-04-29 23:01:28 -0700

    replace spaces with -

--- src/utils/uploadEmoji.ts
+++ src/utils/uploadEmoji.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { ConcurrencyManager } from "axios-concurrency";
-import { v4 as uuidv4 } from "uuid";
+import axios from 'axios';
+import { ConcurrencyManager } from 'axios-concurrency';
+import { v4 as uuidv4 } from 'uuid';
 
-import { getSlackApiData } from "./getSlackApiData";
+import { getSlackApiData } from './getSlackApiData';
 
 const MAX_CONCURRENT_REQUESTS = 1;
 
 const slackApi = axios.create({
-  baseURL: `${window.location.origin}/api`,
+  baseURL: `${window.location.origin}/api`
 });
 
 ConcurrencyManager(slackApi, MAX_CONCURRENT_REQUESTS);
@@ -17,27 +17,28 @@ const NO_OP = function () {};
 export function uploadEmoji(file, callback = NO_OP) {
   const { apiToken, versionUid } = getSlackApiData();
   const timestamp = Date.now() / 1000;
-  const version = versionUid ? versionUid.substring(0, 8) : "noversion";
+  const version = versionUid ? versionUid.substring(0, 8) : 'noversion';
   const id = uuidv4();
-  const name = file.name.split(".")[0].toLowerCase();
+  let fileName = file.name.split('.')[0] ?? '';
+  fileName = fileName.toLowerCase().replace(' ', '-');
 
   const formData = new FormData();
-  formData.append("name", name);
-  formData.append("mode", "data");
-  formData.append("token", apiToken);
-  formData.append("image", file);
+  formData.append('name', fileName);
+  formData.append('mode', 'data');
+  formData.append('token', apiToken);
+  formData.append('image', file);
 
   slackApi({
-    method: "post",
+    method: 'post',
     url: `/emoji.add`,
     params: {
-      _x_id: `${version}-${timestamp}`,
+      _x_id: `${version}-${timestamp}`
     },
     data: formData,
     withCredentials: true,
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
   })
     .then((response) => {
       const error = response?.data?.error;
